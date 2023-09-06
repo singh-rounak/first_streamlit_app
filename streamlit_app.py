982d34edc967cb6fe405551b6008e9c8fe5d3403
Rounak <65697913+singh-rounak@users.noreply.github.com>
2023-09-06 01:52:40 -0400

    Update streamlit_app.py
    streamlit alias stl

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
-import streamlit
-streamlit.title("My Parent's New Healthy Diner")
+import streamlit as stl
+stl.title("My Parent's New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
+stl.header('Breakfast Menu')
+stl.text('🥣 Omega 3 and Blueberry Oatmeal')
+stl.text('🥗 Kale, Spinach & Rocket Smoothie')
+stl.text('🐔 Hard Boiled Free-Range Egg')
+stl.text('🥑🍞 Avacado Toast')
    
-streamlit.header('🥝🍇 Build Your Own Fruit Smoothie 🍌🥭')
+stl.header('🥝🍇 Build Your Own Fruit Smoothie 🍌🥭')
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+stl.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
