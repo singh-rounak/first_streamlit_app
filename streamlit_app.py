fd8d273e98333fe48d6eacbac4f6780d4d40b9d0
Rounak <65697913+singh-rounak@users.noreply.github.com>
2023-09-06 17:41:53 -0400

    Update streamlit_app.py
    added new header - fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 stl.dataframe(fruits_to_show)
 
+# New Section to display fruityvice api response
+stl.header('Fruityvice Fruit Advice!')
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 stl.text(fruityvice_response)
