b579c61dd8b38e494f35057455b721fa7c011b60
Rounak <65697913+singh-rounak@users.noreply.github.com>
2023-09-07 20:44:30 -0400

    Update streamlit_app.py
    Made changes to entire code

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,21 +28,17 @@ stl.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 stl.header('Fruityvice Fruit Advice!')
+try:
+	fruit_choice = stl.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		stl.error("Please select a fruit to get information")
+	else:
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+		fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) #normalize the json version of response
+		stl.dataframe(fruityvice_normalized) #Output of normalization
 
-fruit_choice = stl.text_input('What fruit would you like information about?','Kiwi')
-stl.write('The user entered ', fruit_choice)
-
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#stl.text(fruityvice_response.json()) #just writes the data to the screen.
-
-#normalize the json version of response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-#Output of normalization
-stl.dataframe(fruityvice_normalized)
-
+except URLError as e:
+	stl.error()
 #don't run anything past here while we troubleshoot
 stl.stop()
 
